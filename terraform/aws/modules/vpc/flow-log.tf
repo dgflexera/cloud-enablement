5d13040462f908dc877cb5eb46289020bf9a72b4
Joe Steffee <jsteffee@flexera.com>
2019-03-12 20:04:14 +0000

    log_destination requires an ARN not name

--- terraform/aws/modules/vpc/flow-log.tf
+++ terraform/aws/modules/vpc/flow-log.tf
@@ -1,5 +1,5 @@
 resource "aws_flow_log" "vpc_flow_log" {
-  log_destination = "${aws_cloudwatch_log_group.vpc_flow_log.name}"
+  log_destination = "${aws_cloudwatch_log_group.vpc_flow_log.arn}"
   iam_role_arn   = "${aws_iam_role.vpc_flow.arn}"
   vpc_id         = "${aws_vpc.main.id}"
   traffic_type   = "ALL"
